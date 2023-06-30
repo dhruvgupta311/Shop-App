16287409163f16d6dc9bce7b54b7527209960ef6
Dhruv <guptadhruv1908@gmail.com>
2023-06-30 20:05:58 +0530

    state management completed

--- shop_app/lib/widget/orders_item.dart
+++ shop_app/lib/widget/orders_item.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import '../provider/orders.dart' as ord;
 import 'package:intl/intl.dart';
+import 'dart:math';
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order; // isme jo orderitem h wo orders.dart ka h isiliye as ord import kiya h
   OrderItem(this.order);
-  
+
+  @override
+  State<OrderItem> createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
 // how each order will look like it will be stored in a card.
+var _expanded=false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,14 +21,49 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            title: Text('\$${order.amount}'),
-            subtitle: Text(DateFormat('dd MM yyyy hh:mm').format(order.datetime),),
+            title: Text('\$${widget.order.amount}',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+               ),),
+            subtitle: Text(
+              DateFormat('dd MM yyyy hh:mm').format(widget.order.datetime),
+               style: TextStyle(
+                fontWeight: FontWeight.bold,
+               ),
+              ),
             trailing: IconButton(
-            onPressed:(){}, 
+            onPressed:(){
+              setState(() {
+                _expanded=!_expanded;
+              });
+            }, 
              icon: Icon(
-              Icons.expand_more_rounded,
+              _expanded?  Icons.expand_less : Icons.expand_more,
               ),
              ),
+          ),
+          if(_expanded) Container(
+            padding: EdgeInsets.symmetric(horizontal: 15,vertical: 4),
+            height: min(widget.order.products.length*20.0 +10,180),
+            child: ListView(
+              // maps take the value which runs on the evry item index
+              children: widget.order.products.map((prod)=>
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(prod.title,style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),),
+                  Text('\$${prod.quantity}x \$${prod.price}',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                 ),
+                ],
+              )).toList(),
+            ),
           ),
         ],
       ),
